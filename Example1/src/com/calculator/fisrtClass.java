c45253fc932e4af3c11336dd88580d68d71085c6
Muthukumaran <muthukumaran.s1@tcs.com>
2017-04-24 10:15:21 +0530

    First commit

---
+++ Example1/src/com/calculator/fisrtClass.java
@@ -0,0 +1,14 @@
+package com.calculator;
+
+import javax.jws.*;
+
+@WebService
+public class fisrtClass {
+	
+	@WebMethod
+	public int add (int num1, int num2 ){
+		
+		return num1 + num2;
+	}
+	
+}
